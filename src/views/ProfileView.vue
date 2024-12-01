221725cd91648297b53a1fea4216546865091771
Hoàng Cao Minh <hoangcaominh1812@gmail.com>
2024-12-01 19:09:51 +0700

    Move public pgp key to the bottom

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -9,11 +9,6 @@
     <p>Date of starting playing Touhou: 8 February 2015</p>
     <p>Currently playing: Yes</p>
     <br />
-    <a
-      href="https://keys.openpgp.org/vks/v1/by-fingerprint/22F797E93CDEB10EB7503F915DA65C224F864E9C"
-      >Public PGP Key</a
-    ><br />
-    <br />
     <a href="https://www.youtube.com/channel/UCIIZj54ucp_lKOjBqXuF4vQ" target="_blank"
       ><img class="inline" width="16" src="@/assets/icons/youtube.png" /> Youtube</a
     ><br />
@@ -27,6 +22,11 @@
       ><img class="inline" width="16" src="@/assets/icons/github.png" /> GitHub</a
     ><br />
     <img class="inline" width="16" src="@/assets/icons/discord.png" /> hoangcaominh<br />
+    <br />
+    <a
+      href="https://keys.openpgp.org/vks/v1/by-fingerprint/22F797E93CDEB10EB7503F915DA65C224F864E9C"
+      >Public PGP Key</a
+    ><br />
     <br />
     <PageBackButton></PageBackButton>
   </div>
